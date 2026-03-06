7e0ee36127e8db6a4e86267cb95f0669f4721afc
tamax9717 <tamax9717@gmail.com>
2026-03-06 04:32:00 +0100

    fixing Spring test by moccking it

--- src/test/kotlin/com/example/complaint/ComplaintApplicationTests.kt
+++ src/test/kotlin/com/example/complaint/ComplaintApplicationTests.kt
@@ -2,12 +2,45 @@ package com.example.complaint
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.kafka.KafkaContainer
+import org.testcontainers.utility.DockerImageName
 
-@SpringBootTest
+@SpringBootTest(properties = [
+    "spring.jpa.hibernate.ddl-auto=none",
+    "spring.flyway.enabled=false"
+])
+@Testcontainers
 class ComplaintApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    companion object {
+        @Container
+        @JvmStatic
+        val kafka = KafkaContainer(DockerImageName.parse("apache/kafka"))
 
+        @Container
+        @JvmStatic
+        val postgres = PostgreSQLContainer("postgres:15-alpine")
+            .apply {
+                withDatabaseName("complaints")
+                withUsername("test")
+                withPassword("test")
+            }
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun configureProperties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers)
+            registry.add("spring.datasource.url", postgres::getJdbcUrl)
+            registry.add("spring.datasource.username", postgres::getUsername)
+            registry.add("spring.datasource.password", postgres::getPassword)
+        }
+    }
+
+    @Test
+    fun contextLoads() {}
 }
